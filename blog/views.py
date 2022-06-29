8db121e098fcf655bdd6dbdbb988aa5ef140faaf
Sonola Moyo <sonolamoyosoluwalorun@gmail.com>
2022-06-29 06:17:51 +0100

    link app templates

--- blog/views.py
+++ blog/views.py
@@ -7,26 +7,30 @@ from blog.models import Post
 
 class PostListView(ListView):
     model = Post
-    template_name = ""
+    template_name = "blog.post_list"
 
 
 class PostCreateView(CreateView):
     model = Post
     fields = "__all__"
+    template_name = "blog.post_form"
     success_url = reverse_lazy("blog:all")
 
 
 class PostDetailView(DetailView):
     model = Post
+    template_name = "blog.post_detail"
 
 
 class PostUpdateView(UpdateView):
     model = Post
     fields = "__all__"
+    template_name = "blog.post_form"
     success_url = reverse_lazy("blog:all")
 
 
 class PostDeleteView(DeleteView):
     model = Post
     fields = "__all__"
+    template_name = "blog.post_confirm_delete"
     success_url = reverse_lazy("blog:all")
